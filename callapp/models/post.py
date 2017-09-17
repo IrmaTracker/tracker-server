88228f2a6287cc56a46d8627feb47118e42d6c63
Lemuel Boyce <lemuelboyce@gmail.com>
2017-09-17 00:39:54 -0400

    remove url validation

--- callapp/models/post.py
+++ callapp/models/post.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     name = models.CharField("Name", max_length=255, null=True, blank=True)
-    link = models.URLField("Link", null=True, blank=True)
+    link = models.CharField("Link", max_length=255, null=True, blank=True)
     time = models.CharField("Time", max_length=75, null=True, blank=True)
     status = models.CharField("Status", max_length=255, null=True, blank=True)
     created_on = models.DateTimeField("Created on", auto_now_add=True, editable=False)
