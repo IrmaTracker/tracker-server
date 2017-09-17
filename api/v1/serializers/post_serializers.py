7f46c3bfeecba2730a1d096bb7362b94a2e9c80d
Lemuel Boyce <lemuelboyce@gmail.com>
2017-09-17 01:13:49 -0400

    add place to post

--- api/v1/serializers/post_serializers.py
+++ api/v1/serializers/post_serializers.py
@@ -6,4 +6,4 @@ class PostSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Post
-        fields = ('name', 'link', 'time', 'status')
+        fields = ('name', 'link', 'place', 'time', 'status')
--- callapp/models/post.py
+++ callapp/models/post.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     name = models.CharField("Name", max_length=255, null=True, blank=True)
     link = models.CharField("Link", max_length=255, null=True, blank=True)
     time = models.CharField("Time", max_length=75, null=True, blank=True)
+    place = models.CharField("Place", max_length=255, null=True, blank=True)
     status = models.CharField("Status", max_length=255, null=True, blank=True)
     created_on = models.DateTimeField("Created on", auto_now_add=True, editable=False)
 
